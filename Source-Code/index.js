e89f1492edd5697dfc84d4e8132727f149402ae6
Muhammad Moeez Khan <85444389+MuhammadMoeezKhan@users.noreply.github.com>
2022-01-21 15:15:53 -0500

    Add files via upload

---
+++ Source-Code/index.js
@@ -0,0 +1,58 @@
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        clicked(this.innerHTML);
+        flash(this.innerHTML);
+    });
+}   
+
+document.addEventListener("keydown",function(event){
+    clicked(event.key);
+    flash(event.key);
+});
+
+function clicked(key){
+switch (key) {
+    case "w":
+        var audio = new Audio("sounds/crash.mp3")
+        audio.play();
+        break;
+    case "a":
+        var audio = new Audio("sounds/kick-bass.mp3")
+        audio.play();
+        break;
+    case "s":
+        var audio = new Audio("sounds/snare.mp3")
+        audio.play();
+        break;
+    case "d":
+        var audio = new Audio("sounds/tom-1.mp3")
+        audio.play();
+        break;
+    case "j":
+        var audio = new Audio("sounds/tom-2.mp3")
+        audio.play();
+        break;
+    case "k":
+        var audio = new Audio("sounds/tom-3.mp3")
+        audio.play();
+        break;
+    case "l":
+        var audio = new Audio("sounds/tom-4.mp3")
+        audio.play();
+        break;
+    default:
+        break;
+}
+}
+
+
+function flash(eventKey){
+    var eventButton = document.querySelector("." + eventKey);
+    eventButton.classList.add("pressed");
+
+    setTimeout(function(){
+        eventButton.classList.remove("pressed")
+    },100);
+}
+
